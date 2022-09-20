b96310f8bf4f64e8b28dc11975a721b00d71a297
Juan Roman Brugnoni <jromanbrugno@gmail.com>
2022-09-20 15:03:22 -0300

    oracion

--- Oracion/Oracion.java
+++ Oracion/Oracion.java
@@ -1,2 +1,68 @@
-package Oracion;public class Oracion {
+package Oracion;
+
+public class Oracion {
+//    Atributos de instacia
+    char s[];
+
+//    constructor
+    public Oracion(String str){
+        s = new char[str.length()];
+        for(int i=0; i<s.length;i++){
+            s[i] = str.charAt(i);
+        }
+    }
+
+//    Comandos
+    public void reducirBlancos(){
+        int i = 1,cantBlanco = 1;
+        boolean blanco = false;
+
+        while((i<s.length)&&(!blanco)){
+//            Si hay 2 espacios en blanco
+            if((s[i-1]==' ')&&(s[i]==' ')){
+                cantBlanco++;
+//                Reescribo el arreglo
+                for(int j=i;j<s.length;j++){
+                    s[j-1] = s[j];
+                }
+            }else i++;
+//            Veo la cantidad de espacios blanco al final del arreglo
+            if(cantBlanco==(s.length-i)) i=s.length;
+        }
+
+    }
+
+    public void establecerLetra(int pos,char l){
+        s[pos] = l;
+    }
+
+//    consultas
+    public char obtenerLetra(int pos){
+        return s[pos];
+    }
+    public boolean esLetra(int pos){
+        boolean esLetra = false;
+        if(pos<s.length) esLetra = s[pos] != ' ';
+
+        return esLetra;
+    }
+
+    public boolean esPrimeraPalabra(int pos){
+        boolean esPrimeraPalabra = false;
+        if(pos<s.length) esPrimeraPalabra = s[pos] != ' ';
+
+        return esPrimeraPalabra;
+    }
+
+    public int longitud(){
+        return s.length;
+    }
+    public void print(){
+        for(int i = 0; i<s.length;i++){
+            System.out.print(s[i]);
+        }
+        System.out.println();
+    }
+
+
 }
--- Oracion/TesterOracion.java
+++ Oracion/TesterOracion.java
@@ -1,2 +1,13 @@
-package Oracion;public class TesterOracion {
+package Oracion;
+
+public class TesterOracion {
+    public static void main(String args[]){
+
+        Oracion oracion1 = new Oracion("que     onda    pibe ");
+        oracion1.print();
+
+        oracion1.reducirBlancos();
+
+        oracion1.print();
+    }
 }
